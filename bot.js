6c5051507052c9f105a1141b9e510973650a585c
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2019-05-29 23:31:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,14 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', function(message){
-    if(message.content.endsWith('2!'))
+    if(message.content.endsWith(' 2!'))
+    {
+        message.channel.send({files: ["https://i.imgur.com/5VLqjJx.gif"]});
+    }
+});
+
+bot.on('message', function(message){
+    if(message.toLowerCase.content.endsWith(' two!'))
     {
         message.channel.send({files: ["https://i.imgur.com/5VLqjJx.gif"]});
     }
